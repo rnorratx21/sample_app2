a59356e30809266b514108023849f0ff82f61004
Rick Orr <orr.rick@gmail.com>
2013-04-18 00:09:47 -0500

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp2::Application.routes.draw do
+  resources :users
 
-  get "users/new"
+  
 
   root to: 'static_pages#home'
 
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name 		"Rick Orr"
+		email		"orr.rick@gmail.com"
+		password	"Monkey21"
+		password_confirmation "Monkey21"
+	end
+end
